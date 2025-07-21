abef9bbaaca138220e77524de1c089fe5ba81376
Serghei <akaindark@gmail.com>
2025-07-22 01:06:28 +0300

    add App Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 import useTelegram from './hooks/useTelegram'
+import Header from './components/Header/Header'
 
 function App() {
   const { onToggleButton, tg } = useTelegram()
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <>
+      <Header />
       <button onClick={onToggleButton}>Toggle</button>
     </>
   )
