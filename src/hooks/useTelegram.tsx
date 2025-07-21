fb08857dfa2e65e4fa8ab9ec127e393fd8b80e44
Serghei <akaindark@gmail.com>
2025-07-22 00:48:12 +0300

    fix toggleButton

--- src/hooks/useTelegram.tsx
+++ src/hooks/useTelegram.tsx
@@ -4,9 +4,9 @@ const useTelegram = () => {
   const onClose = () => tg.close()
   const onToggleButton = () => {
     if (tg.MainButton.isVisible) {
-      tg.MainButton.show()
-    } else {
       tg.MainButton.hide()
+    } else {
+      tg.MainButton.show()
     }
   }
   return {
