7697fd5b6d1dc15ea06d32beb704c9c61ae7d92c
Serghei <akaindark@gmail.com>
2025-07-30 00:19:10 +0300

    test style button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,12 @@
-//import './Button.css'
-
 const Button: React.FC<
   React.ButtonHTMLAttributes<HTMLButtonElement>
 > = props => {
-  return <button className={`button ${props.className}`} {...props} />
+  return (
+    <button
+      className={`text-[var(--tg-theme-button-text-color)] bg-[var(--tg-theme-button-color)] px-3 py-2 ${props.className}`}
+      {...props}
+    />
+  )
 }
 
 export default Button
